0c557399ee324b0bae458543638d0c76735aaeb1
frederick tyteca <frederick@tyteca.net>
2021-03-15 23:12:42 +0100

    adding web service and tailwind

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="bg-gray-800 text-white mt-32">
+    <div class="container mx-auto p-6 md:pt-10 md:pb-10">
+        <div class="border-b border-white">
+            <div class="-mx-3 pb-5">
+                
+            </div>
+        </div>
+        <div class="lg:flex -mx-6 mt-8">
+            
+        </div>
+        <p class="mt-2 text-sm">
+            © 2021 GPU Dispo. All rights reserved.
+        </p>
+    </div>
+</div>
